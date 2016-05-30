31304f127e3850d68fb4b42e0cf01d0f6b8fea48
youclavier <youclavier@gmail.com>
2016-05-30 17:03:55 +0800

    save

--- src/scope.js
+++ src/scope.js
@@ -1,6 +1,10 @@
 'use strict';
 var _ = require('lodash');
 
+function initWatchVal() {
+
+}
+
 function Scope() {
 	this.$$watchers = [];
 }
@@ -10,21 +14,31 @@ module.exports = Scope;
 Scope.prototype.$watch = function (watchFn, listenerFn) {
 	var watcher = {
 		watchFn: watchFn,
-		listenerFn: listenerFn
+		listenerFn: listenerFn || function() {},
+		last: initWatchVal
 	};
 	this.$$watchers.push(watcher);
 };
 
-Scope.prototype.$digest = function () {
+Scope.prototype.$$digestOnce = function () {
 	var self = this;
-	var newValue, oldValue;
+	var newValue, oldValue, dirty;
 
 	_.forEach(this.$$watchers, function (watcher) {
 		newValue = watcher.watchFn(self);
 		oldValue = watcher.last;
 		if (newValue !== oldValue) {
 			watcher.last = newValue;
-			watcher.listenerFn(newValue, oldValue, self);
+			watcher.listenerFn(newValue, (oldValue === initWatchVal ? newValue : oldValue), self);
+			dirty = true;
 		}
 	});
+	return dirty;
+};
+
+Scope.prototype.$digest = function () {
+	var dirty;
+	do {
+		dirty = this.$$digestOnce();
+	} while (dirty);
 };
